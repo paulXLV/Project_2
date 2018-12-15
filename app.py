3868dca02bc8937fffe3ad5038983913d2cbeb60
Baconmania31 <isaacbmuck@gmail.com>
2018-12-15 15:50:33 -0500

    fixed flask

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # import necessary libraries
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import json
 import pymongo
-import jsonify
+import pandas as pd
+import os
+import csv
 
 # create instance of Flask app
 
@@ -17,22 +19,23 @@ app = Flask(__name__)
 conn = "mongodb://localhost:27017/tech_app"
 client = pymongo.MongoClient(conn)
 db = client.zipDB
-db.drop()
+#db.zipDB.drop()
 all_data = "zipcode_sf_zri_2018.csv"
 all_data_pd = pd.read_csv(all_data)
 zipData = json.loads(all_data_pd.to_json(orient='records'))
-db.zipDB.insert_many(zipData)
+db.zipColl.insert_many(zipData)
 
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("heatmap.html")
     
-@app.route("/<year>")
-def samples(year):
-    filterData = db.zipDB.find({"year": year)
-    return(jsonify(filterData))
+@app.route("/zipjson")
+def samples():
+    filterData = list(db.zipDB.find())
+#     return jsonify([zip for zip in filterData])
+    return filterData
                                 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
