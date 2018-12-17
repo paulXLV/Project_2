62ae82765160264cf2e855b466fa43a29eccee7e
Bryan Barrows <bryan.barrows1@gmail.com>
2018-12-17 18:28:18 -0500

    updated legend

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,8 +1,8 @@
 // Create the tile layer that will be the background of our map
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
   maxZoom: 18,
-  id: "mapbox.light",
+  id: "mapbox.streets",
   accessToken: API_KEY
 });
 
@@ -35,8 +35,31 @@ var overlays = {
   "Facebook": layers.FACEBOOK,
 };
 
+
+// create separate layer groups for each year
+var eleven = L.layerGroup(eleven)
+var twelve = L.layerGroup(twelve)
+var thirteen = L.layerGroup(thirteen)
+var fourteen = L.layerGroup(fourteen)
+var fifteen = L.layerGroup(fifteen)
+var sixteen = L.layerGroup(sixteen)
+var seventeen = L.layerGroup(seventeen)
+var eighteen = L.layerGroup(eighteen)
+
+// create overylays
+var overlayMaps = {
+  2011 : eleven,
+  2012 : twelve,
+  2013: thirteen,
+  2014: fourteen,
+  2015: fifteen,
+  2016: sixteen,
+  2017: seventeen,
+  2018: eighteen
+}
+
 // Create a control for our layers, add our overlay layers to it
-L.control.layers(null, overlays).addTo(map);
+L.control.layers(overlayMaps, overlays).addTo(map);
 
 // Create a legend to display information about our map
 var info = L.control({
@@ -52,44 +75,41 @@ info.onAdd = function() {
 // Add the info legend to the map
 info.addTo(map);
 
+// define arrays to hold markers
+// var 2011 = []
+// var 2012 = []
+// var 2013 = []
+// var 2014 = []
+// var 2015 = []
+// var 2016 = []
+// var 2017 = []
+// var 2018 = []
+
+// var url = "../all_zip_cities_years.csv";
+
+// loop thru locations and create markers 
+d3.csv("../all_zip_cities_years.csv", function(response) {
+  // var year = zipData.year
+
+  console.log(response);
+
+  var heatArray = [];
+
+  for (var i = 0; i <  response.length; i++) {
+    var location = ([response[i].lat, response[i].lon, response[i].ZRI]);
+
+    if (location) {
+      heatArray.push([location[0],location[1],location[2]]);
+      };
+  }
+
+  console.log(heatArray)
+
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  }).addTo(map);
+});
 
 
 
-
-
-
-
-// function createMarkers(response) {
-//   var locations = response.data
-// } 
-
-
-
-
-
-
-// var url = "";
-
-// var 
-
-
-// d3.json(url, function(response) {
-
-//   console.log(response);
-
-//   var heatArray = [];
-
-//   for (var i = 0; i < response.length; i++) {
-//     var location = response[i].location;
-
-//     if (location) {
-//       heatArray.push([location.coordinates[1], location.coordinates[0]]);
-//     }
-//   }
-
-//   var heat = L.heatLayer(heatArray, {
-//     radius: 20,
-//     blur: 35
-//   }).addTo(myMap);
-
-// });
